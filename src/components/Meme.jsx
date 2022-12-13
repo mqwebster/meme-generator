8c3b71158ab7e1f29527575c6198e1b2733b291b
Marques Webster <marques.q.webster@gmail.com>
2022-12-13 15:25:12 -0600

    random image state change setup

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,34 +1,46 @@
-import React from 'react';
+import { useState } from 'react';
+import memes from '/src/memesData.js';
 import './css/Meme.css';
 
 export default function Meme() {
+  const [memeImage, setMemeImage] = useState('');
+
+  const getMemeImage = () => {
+    const memesList = memes.data.memes;
+    const randNum = Math.floor(Math.random() * memesList.length);
+    setMemeImage(memesList[randNum].url);
+  };
+
   return (
     <div className='meme-form'>
-      <form>
-        <div className='meme-form__inputs'>
-          <input
-            type='text'
-            name='input-top'
-            placeholder='Meme Upper Text'
-            className='meme-form__input top'
-          />
-          <input
-            type='text'
-            name='input-bottom'
-            placeholder='Meme Lower Text'
-            className='meme-form__input bottom'
-          />
-        </div>
+      <div className='meme-form__inputs'>
+        <input
+          type='text'
+          name='input-top'
+          placeholder='Meme Upper Text'
+          className='meme-form__input top'
+        />
+        <input
+          type='text'
+          name='input-bottom'
+          placeholder='Meme Lower Text'
+          className='meme-form__input bottom'
+        />
+      </div>
 
-        <div className='meme-form__submit'>
-          <input
-            type='submit'
-            name='submit'
-            value='Get a new meme image 🖼'
-            className='meme-form__btn'
-          />
-        </div>
-      </form>
+      <div className='meme-form__submit'>
+        <input
+          type='button'
+          name='button'
+          value='Get a new meme image 🖼'
+          className='meme-form__btn'
+          onClick={getMemeImage}
+        />
+      </div>
+
+      <div className='meme-img'>
+        <img src={memeImage} className='meme-img__inner' />
+      </div>
     </div>
   );
 }
